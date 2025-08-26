beda7d04d80b97dc00b3c2e411a5bb5739b51f45
SiphiweRadebe <siphiwemoeketsi1@gmail.com>
2025-08-26 22:00:04 +0200

    Dev

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SipTestProject.Pages
+{
+    public class LoginPage
+    {
+        private IWebDriver _driver;
+        [FindsBy(How = How.Id, Using = "Input_Email")]
+        private IWebElement emailItxt;
+        [FindsBy(How = How.Id, Using = "Input_Password")]
+        private IWebElement passwordtxt;
+        [FindsBy(How = How.Id, Using = "login-submit")]
+        private IWebElement loginbtn;
+
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(_driver, this);
+        }
+        public void Login(string username, string password)
+        {
+            emailItxt.SendKeys(username);
+            passwordtxt.SendKeys(password);
+            loginbtn.Click();
+        }
+    }
+}
---
+++ Pages/LoginTest1.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SipTestProject.Pages
+{
+    public class LoginTest1
+    {
+        private IWebDriver _driver;
+        [SetUp]
+        public void setup()
+        {
+            _driver = new ChromeDriver();
+        }
+
+
+        [Test]
+        public void Login()
+        {
+            string? User = TestContext.Parameters["User"];
+            string? Password = TestContext.Parameters["Password"];
+            LoginPage lp = new LoginPage(_driver);
+
+            _driver.Navigate().GoToUrl(TestContext.Parameters["Login_url"]);
+            lp.Login(User, Password);    
+            //Assert.Pass();
+
+        }
+
+        [TearDown]
+        public void teardown()
+        {
+            _driver.Quit();
+            _driver.Dispose();  
+        }
+
+    }
+   } 
